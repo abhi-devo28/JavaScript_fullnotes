ce0a8d7480851edf5e28d5d5d08227df9f707a97
abhi-devo28 <abhishek07th@gmail.com>
2023-10-03 23:55:56 +0530

    javascript notes

---
+++ 09_Date_Time/09_Date and Time,.js
@@ -0,0 +1,9 @@
+ // --------------------- DATE -------------------
+
+ let myDate = new Date()
+ 
+ console.log(myDate)
+ console.log(myDate.toLocaleString())
+ console.log(myDate.toString())
+ console.log(myDate.toJSON())
+ console.log(typeof myDate)
