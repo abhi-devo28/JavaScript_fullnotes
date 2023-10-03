d74782d4e7ef6293f7f7911a6c75edc693fc50e8
abhi-devo28 <abhishek07th@gmail.com>
2023-10-04 00:32:30 +0530

    javascript notes

--- 09_Date_Time/09_Date and Time,.js
+++ 09_Date_Time/09_Date and Time,.js
@@ -1,9 +1,21 @@
  // --------------------- DATE -------------------
 
- let myDate = new Date()
- 
+ let myDate =  new Date(2023,0,23)
+
  console.log(myDate)
  console.log(myDate.toLocaleString())
  console.log(myDate.toString())
  console.log(myDate.toJSON())
  console.log(typeof myDate)
+
+ let timeStamp = Date.now()
+
+ //Method for comparing the date in milli second
+
+ console.log(Math.floor((Date.now())/ 1000))
+ console.log(myDate.getTime())
+
+ console.log(new Date(2023,0,23).toLocaleString("default",{
+    weekday: "short",
+    
+ }))
