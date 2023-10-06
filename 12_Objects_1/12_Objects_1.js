88d93395683170e134eef95c5b99bb50a275f10b
abhi-devo28 <abhishek07th@gmail.com>
2023-10-06 19:06:40 +0530

    javascript notes

---
+++ 12_Objects_1/12_Objects_1.js
@@ -0,0 +1,29 @@
+// singleton   "when we declare it as constructor" 
+
+// object literals
+  
+Object.create   // constructor method in which singleton is made
+
+let sym1 = Symbol()
+
+const Jsuser = {
+    name: "abhishek",
+    "full name" : "abhishek thakur",
+   [sym1] : "mykey2",
+    age : 18,
+    email : "abhishek@google.com",
+    isLoggedin : false,
+    lastLoginDays: [ "monday", "tuesday"]
+}
+
+console.log(Jsuser.email)  // we old way to print the object 
+console.log(Jsuser["full name"]) // this way we can use this type of value to print
+console.log(Jsuser[sym1])
+
+// changing the email
+
+Jsuser.email = "tuhar@google.com"
+Object.freeze(Jsuser)  // this is way to freeze the object
+Jsuser.email = "rajput@chatgpt.com"
+
+console.log(Jsuser)
