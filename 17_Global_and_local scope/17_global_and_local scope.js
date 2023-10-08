ee07002c6584ea48c447aecb3c47acbb5db0a8d1
abhi-devo28 <abhishek07th@gmail.com>
2023-10-08 11:41:19 +0530

    javascript notes

---
+++ 17_Global_and_local scope/17_global_and_local scope.js
@@ -0,0 +1,14 @@
+
+let a = 34
+
+if (true){
+    let a = 45
+    const b = 64
+    console.log( `INNER: ${a}`)
+    //var c = 23
+}
+
+console.log(a)
+// console.log(a) 
+ // console.log(b)
+// console.log(c)   // so var is not used in the entire coding of the java script it does not obey scope
