b14f70307355b55e00d0f18f0385c9b94626677a
abhi-devo28 <abhishek07th@gmail.com>
2023-10-08 12:05:50 +0530

    javascript notes

--- 17_Global_and_local scope/17_global_and_local scope.js
+++ 17_Global_and_local scope/17_global_and_local scope.js
@@ -1,14 +1,63 @@
+// ---------------- GLOBAL AND LOCAL SCOPE ---------
 
 let a = 34
 
-if (true){
+if (true) {
     let a = 45
     const b = 64
-    console.log( `INNER: ${a}`)
+    console.log(`INNER: ${a}`)
     //var c = 23
 }
 
 console.log(a)
 // console.log(a) 
- // console.log(b)
+// console.log(b)
 // console.log(c)   // so var is not used in the entire coding of the java script it does not obey scope
+
+
+// ------------------  NESTED SCOPE -----------------
+  //child can acess the  parent code 
+
+function one() {
+    const username = "abhishek"
+
+    function two() {
+        const website = "youtube "
+        console.log(username)
+    }
+    //console.log(website)
+
+    two()
+}
+
+one()
+
+
+//------------------------------------------------
+
+if (true) {
+    const username = "abhishek"
+    if ( username ==="abhishek") {
+            const website = "youtube"
+            console.log(username + website)
+    }
+    //   console.log(website)
+}
+ // console.log( username)
+
+
+ //-----------------------------------------------------
+
+  ///WAYS TO DECLARE THE Function
+
+ console.log(addone(5))
+ function addone(num){
+    return num +1
+ }
+
+
+addtwo(5)
+ const addtwo = function( num ){
+    return num +2
+ }
+  // THERE IS AN ERROR
