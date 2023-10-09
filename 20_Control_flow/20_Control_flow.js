f9a3bc2e624bb201b6baa4b34aa3e0b86633f39d
abhi-devo28 <abhishek07th@gmail.com>
2023-10-09 23:44:04 +0530

    javascript notes

--- 20_Control_flow/20_Control_flow.js
+++ 20_Control_flow/20_Control_flow.js
@@ -93,7 +93,7 @@ if (userLoggedI && debitCard && loggedInfromemail||loggedInfromgoogle ){
 
     //truthy 
 
-    const Useremail = ""
+    const Useremail = []
 
     if (Useremail) {
         console.log( "got the user email")
@@ -110,4 +110,29 @@ if (userLoggedI && debitCard && loggedInfromemail||loggedInfromgoogle ){
 
     // falsy values => //false, 0, -0, 0n, "", null , undefined, NaN
 
-    // truthy values => 
+    // truthy values =>  "0", 'false' , " " , [] , {} , function(){}
+
+    if (Useremail.length === 0 ){
+        console.log("Array is empty")
+    }
+  
+const emptyObj = {}
+
+    if (Object.keys(emptyObj).length === 0) {
+        console.log ( "Object is empty")
+    }
+ // false == 0 , true       false == ''  true     0 == '' ,  true
+
+ // --------------------------------------------------------------
+
+ // nullish coalescing operator ( ?? ) : null defined
+
+ let val1; 
+ val1 = 5 ?? 10
+val1 = null ?? 10  // => 10
+ val1 = undefined ?? 15
+
+ val1 = null ?? 10 ?? 20 
+ console.log(val1)
+
+
