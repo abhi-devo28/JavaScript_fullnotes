897437e69044e7c3ffd50465717359a6fcfc65f6
abhi-devo28 <abhishek07th@gmail.com>
2023-10-11 16:43:12 +0530

    javascript notes

---
+++ 21_ForWhile/21_Forwhile.js
@@ -0,0 +1,6 @@
+// for loop
+
+for (let i = 0; i < 10 ; i++) {       // firstly varibale intialize and then condition check then printing work and before the ending curly brackets increment is done and agian check the condition
+    const element = i;
+    console.log(element)
+}
