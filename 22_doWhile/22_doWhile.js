ce9968d02b6d5e12c3bd71495dfc10ad30812631
abhi-devo28 <abhishek07th@gmail.com>
2023-10-17 19:41:52 +0530

    javascript notes

---
+++ 22_doWhile/22_doWhile.js
@@ -0,0 +1,28 @@
+// while and do while loop 
+
+let index = 0 
+
+   //syntax of while loop
+
+while (index <=10) {
+    console.log(`value of index is ${index}`)
+    index = index + 2
+}
+
+
+let myArray = [ 'abhishek ', 'tushar',' nishant']
+let arr = 0 
+while (arr < myArray.length) { 
+console.log(`value is ${myArray[arr]}`)
+   arr = arr + 1
+}
+ 
+//  do while loop 
+
+let score = 11
+
+do {
+    console.log(`score is ${score}`);
+    score++
+
+} while(score <= 10);
