b69bfa29b27330855c3228f7758103ee0ae00215
abhi-devo28 <abhishek07th@gmail.com>
2023-10-24 20:30:47 +0530

    Project 1

---
+++ Project 1_color changer/script.js
@@ -0,0 +1,8 @@
+const buttons = document.querySelectorAll(".buttons")
+const body = document.querySelectorAll(".body")
+
+// buttons.forEach(function(buttons){
+    // console.log(buttons)
+
+// })
+console.log(buttons)
