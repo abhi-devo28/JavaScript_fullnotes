250af0c0c5a43a00bf16a20481df800d63964ca4
abhi-devo28 <abhishek07th@gmail.com>
2023-10-25 18:57:05 +0530

    javascript notes

--- Project 1_color changer/script.js
+++
@@ -1,8 +0,0 @@
-const buttons = document.querySelectorAll(".buttons")
-const body = document.querySelectorAll(".body")
-
-// buttons.forEach(function(buttons){
-    // console.log(buttons)
-
-// })
-console.log(buttons)
